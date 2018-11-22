000df134c0021deecdfb3981077b303e4ca60579
Rajashekar Reddy <rajashekarreddy1212@gmail.com>
2018-11-22 13:03:45 +0530

    added comment for branch2

--- src/main/java/test/pack/JodaDateTimeUtils.java
+++ src/main/java/test/pack/JodaDateTimeUtils.java
@@ -6,6 +6,7 @@ public class JodaDateTimeUtils {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// Added comment for branch2
 		JodaDateTimeUtils jdt = new JodaDateTimeUtils();
 System.out.println("Hello World!\n"+jdt.jGetYear());
 	}
